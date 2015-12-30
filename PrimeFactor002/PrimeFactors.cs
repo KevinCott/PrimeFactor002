be587124d4803e3b063c0bbdf231620fc9096e92
Kevin <Devin@SPAP>
2015-12-29 23:14:55 -0600

    First test working.

---
+++ PrimeFactor002/PrimeFactors.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeFactor002
+{
+    public class PrimeFactors
+    {
+        public static IEnumerable<int> Generate(int n)
+        {
+            return Enumerable.Empty<int>();
+        }
+    }
+}
---
+++ PrimeFactor002Test/PrimeFactorsTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Xunit;
+using static PrimeFactor002.PrimeFactors;
+
+namespace PrimeFactor002Test
+{
+    public class PrimeFactorsTest
+    {
+        [Fact]
+        public void TestOne()
+        {
+            Assert.Equal(Enumerable.Empty<int>(), Generate(1));
+        }
+    }
+}
